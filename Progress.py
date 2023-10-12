c3f4f8ffd06d6fd3a226372867efb5552e3f0c27
quyansiyuanwang <132176401+quyansiyuanwang@users.noreply.github.com>
2023-10-12 09:56:17 +0800

    add new keywords
    new keywords:
        step
    fixed some bugs

--- Progress.py
+++ Progress.py
@@ -1,17 +1,19 @@
 class Progress:
     def __init__(self, total, 
                     label: str = 'progress:',
+                    step: int = 1, 
                     precision: int = 2, 
                     bar_length: int = 10, 
                     style: str = '=',
                     width_rate: int = 1,
-                    flag: str = 'o'
+                    flag: str = 's'
                     ):
         import time
         self.__total = total
         self.__end = len(self.__total)
         self.__yet = -1
         self.label = label
+        self.log_step = step
         self.precision = precision
         self.bar_length = bar_length
         self.style = style
@@ -36,7 +38,7 @@ class Progress:
         return 0.01
         
     def step(self, flag):
-        rating = '{:4>.3f}s/t' if flag == 'o' else '{:4>.3f}t/s'
+        rating = '{:4>.3f}t/s' if flag == 's' else '{:4>.3f}s/t'
         self.__yet += 1
         shape_length = self.bar_length
         progress = self.progress * self.bar_length
@@ -52,8 +54,8 @@ class Progress:
                             ), 
                 end = '' if self.__totalPrecision != 1 else '\n'
                 )
-        
-        logConsole()
+        if self.__yet % self.log_step == 0:
+            logConsole()
         
     def __iter__(self):
         self.__iterObject = tuple(self.__total)
@@ -70,17 +72,28 @@ class Progress:
     
     
 import time
-st = time.perf_counter()
-for i in Progress(range(1000), flag = 'o'):  # flag default is o
-    time.sleep(0.00001 * i + 0.005)
-print('progress for:', time.perf_counter() - st)
-
-st = time.perf_counter()
-for i in range(1000):
-    time.sleep(0.00001 * i + 0.005)
-print('normal for:', time.perf_counter() - st)
-
-
-for i in Progress(range(1000), flag = 's'):
-    time.sleep(-0.00005 * i + 0.05)
+def run_test():
+    st = time.perf_counter()
+    for i in Progress(range(1000), flag = 'o'):  # flag default is o
+        time.sleep(0.00001 * i + 0.005)
+    print('progress for:', time.perf_counter() - st)
     
+    st = time.perf_counter()
+    for i in range(1000):
+        time.sleep(0.00001 * i + 0.005)
+    print('normal for:', time.perf_counter() - st)
+    
+    
+    for i in Progress(range(1000), flag = 's'):
+        time.sleep(-0.00005 * i + 0.05)
+        
+
+def fibo3(n):
+    i, j = 1, 1
+    for _ in Progress(range(n // 2)):
+        i += j
+        j += i
+    return j if n % 2 == 0 else i
+
+fibo3(1000000)
+print('end')
